9272cf2f0ab4a4581a89727b4eb00ad65b28ec13
HirenKava <hiren.embitel@gmail.com>
2025-08-04 17:39:43 +0530

    Review the Request either accept or reject

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { userModel } from "./user.js"; // Assuming userModel is defined in user.js
 
 const connectionRequestSchema = new mongoose.Schema({
     from_user_id: {
@@ -19,6 +20,43 @@ const connectionRequestSchema = new mongoose.Schema({
 },
 {
     timestamps: true, // Automatically manage createdAt and updatedAt fields
+});
+
+connectionRequestSchema.index({ from_user_id: 1, to_user_id: 1 }, { unique: true }); // Ensure unique connection requests between users  
+
+connectionRequestSchema.pre("save",async function(next) {
+
+    const isExistingFromUser = await userModel.exists({ _id: this.from_user_id });
+    if (!isExistingFromUser) {
+        return next(new Error("from_user_id must be valid users"));
+    }
+    const isExistingToUser = await userModel.exists({ _id: this.to_user_id });
+    if (!isExistingToUser) {
+        return next(new Error("to_user_id must be valid users"));
+    }
+    // Ensure that the status is always set to 'pending' when a new request is created
+    // check from user id and to user id are not same
+    // if(this.from_user_id.toString() === this.to_user_id.toString()) {
+    //     return next(new Error("From user and To user cannot be the same"));
+    // }
+    // check request is not duplicate
+    const existingRequest = await this.constructor.findOne({
+        $or: [
+            { from_user_id: this.from_user_id, to_user_id: this.to_user_id,status: this.status },
+            { from_user_id: this.to_user_id, to_user_id: this.from_user_id, status: this.status }
+        ]
+    });   
+    console.log(existingRequest);
+    if (existingRequest) {
+        return next(new Error("Connection request already exists with the same status"));
+    }
+    // console.log(existingRequest);
+    // var nextProcedureAllowedStatuses = ["interested","ignored"/*"accepted", "rejected"*/];
+    // if (!existingRequest && !nextProcedureAllowedStatuses.includes(this.status)) {
+    //    // console.log("Invalid status provided. Valid statuses are: ", nextProcedureAllowedStatuses);
+    //     return next(new Error(`Invalid status providedddd. Valid statuses are: ${nextProcedureAllowedStatuses.join(", ")}`));
+    // }
+    next();
 });
 
 export const connectionRequestModel = mongoose.model("connection_request", connectionRequestSchema);
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,20 +1,38 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.js";
 import { connectionRequestModel } from "../models/connectionRequest.js";
+import { userModel } from "../models/user.js";
 
 const requestRouter = express.Router();
 
 requestRouter.post("/request/send/:status/:userId", authMiddleware, async (req, res) => {
+    //"interested", "ignored","accepted", "rejected"
     try {
         var allowedStatuses = ["interested", "ignored"];
         if (!allowedStatuses.includes(req.params.status)) {
             return res.status(400).json({ valid: false, message: "Invalid status provided" });
         }
-        //throw error if request is duplicate
-        const existingRequest = await connectionRequestModel.findOne({from_user_id:req.user._id, to_user_id: req.params.userId, status: req.params.status});
-        if (existingRequest) {
-            return res.status(400).json({ valid: false, message: "Connection request already exists with the same status" });
+
+        if(req.params.userId === req.user._id.toString()) {
+            return res.status(400).json({ valid: false, message: "You cannot send a connection request to yourself" });
         }
+        // Check if the userId is valid
+        // const toUserId = req.params.userId;
+        // const isExistingUser = await userModel.exists({ _id: toUserId });
+        // if (!isExistingUser) {
+        //     return res.status(404).json({ valid: false, message: "User not found" });
+        // }
+        //throw error if request is duplicate
+        // const existingRequest = await connectionRequestModel.findOne({
+        //     $or: [
+        //         { from_user_id: req.user._id, to_user_id: req.params.userId, status: req.params.status },
+        //         { from_user_id: req.params.userId, to_user_id: req.user._id, status: req.params.status }
+        //     ]
+        //     //from_user_id:req.user._id, to_user_id: req.params.userId, status: req.params.status
+        // });
+        // if (existingRequest) {
+        //     return res.status(400).json({ valid: false, message: "Connection request already exists with the same status" });
+        // }
         const connectionRequest = new connectionRequestModel({
             from_user_id: req.user._id,
             to_user_id: req.params.userId,
@@ -24,7 +42,37 @@ requestRouter.post("/request/send/:status/:userId", authMiddleware, async (req, 
         res.status(201).json({ valid: true, message: "Connection request sent successfully  as interested.", request: savedRequest });
     }catch (error) {
         //console.error("Error sending connection request:", error);
-        res.status(500).json({valid:false, message: "Internal server error" });
+        res.status(500).json({valid:false, message: error.message || "An error occurred while sending the connection request."});
+    }
+});
+
+requestRouter.post("/request/review/:status/:requestId", authMiddleware, async (req, res) => {
+    try {
+        const allowedStatuses = ["accepted", "rejected"];
+        if (!allowedStatuses.includes(req.params.status)) {
+            return res.status(400).json({ valid: false, message: "Invalid status provided" });
+        }
+        const request = await connectionRequestModel.findOne({
+            from_user_id:req.params.requestId,
+            to_user_id: req.user._id,
+            status: "interested"
+        });
+        console.log(request);
+        if (!request) {
+            return res.status(404).json({ valid: false, message: "Connection request not found OR Already accepted or rejected or ignored the request." });
+        }
+        // Check if the user is authorized to review this request
+        console.log(request.to_user_id.toString() +"!=="+ req.user._id.toString());
+        if (request.to_user_id.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ valid: false, message: "You are not authorized to review this request" });
+        }
+        // Update the status of the connection request
+        request.status = req.params.status;
+        const updatedRequest = await request.save();
+        res.status(200).json({ valid: true, message: `Connection request reviewed as ${req.params.status}.`, request: updatedRequest });
+    } catch (error) {
+        console.error("Error reviewing connection request:", error);
+        res.status(500).json({ valid: false, message: error.message || "An error occurred while reviewing the connection request." });
     }
 });
 
